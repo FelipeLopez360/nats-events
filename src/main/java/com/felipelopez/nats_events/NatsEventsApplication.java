05ceb766a282548696a344c4707520e834327e20
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-09-23 22:29:42 -0500

    first commit

---
+++ src/main/java/com/felipelopez/nats_events/NatsEventsApplication.java
@@ -0,0 +1,13 @@
+package com.felipelopez.nats_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsEventsApplication.class, args);
+	}
+
+}
